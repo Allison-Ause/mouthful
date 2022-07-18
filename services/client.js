6fd653b12e35dda63112f323ef3631672f2cc056
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-07-17 17:52:40 -0700

    Sketch out word-service module

--- services/client.js
+++ services/client.js
@@ -2,3 +2,12 @@ const SUPABASE_URL = 'https://yavjmsfnxelnvzpgaxrb.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlhdmptc2ZueGVsbnZ6cGdheHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc5Mjg5MjEsImV4cCI6MTk3MzUwNDkyMX0.u0Rn9ZsRbvg8dVVJSBHNZZ2D6m-UpIooFkL9zRiry6M';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function checkResponse(response) {
+    if (response.error) {
+        // eslint-disable-next-line no-console
+        console.trace(response.error);
+        return null;
+    }
+    return response.data;
+}
---
+++ services/word-service.js
@@ -0,0 +1,23 @@
+import { client, checkResponse } from './client.js';
+
+export async function getWords() {
+    // TODO: get all words and return an array
+    // on error, return null
+}
+
+export async function getWord(wordId) {
+    // TODO: get the word that matches wordId
+    // join on foreign table relationship to get profiles
+    // on error, return null
+}
+
+export async function getProfile(userId) {
+    // TODO: get the profile from user id
+    // on error, return null
+}
+
+export async function getProfileWithSavedWords(userId) {
+    // TODO: get the profile from user id
+    // join on foreign table relationship to get saved words
+    // on error, return null
+}
