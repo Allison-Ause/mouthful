986acadd21893e51bf79505a772d1c9b688a0f3b
Dave Fisher <dave.j.fish@gmail.com>
2022-07-18 11:58:53 -0700

    handleAddWord and handleRemoveWord functionality added

--- detail/detail.js
+++ detail/detail.js
@@ -1,5 +1,5 @@
 import { getUser, signOut } from '../services/auth-service.js';
-import { protectPage } from '../utils.js';
+import { getWordByID, protectPage } from '../utils.js';
 import createUser from '../components/User.js';
 import { getProfile, getWord } from '../services/word-service.js';
 import createSingleServing from '../components/singleServing.js';
@@ -26,17 +26,17 @@ async function handleSignOut() {
 }
 
 async function handleRemoveWord(word_id) {
-    console.log('removing');
-    console.log('word id: ', word_id, 'profile id: ', profile.id);
-    const data = await removeWord(word_id, profile.id);
+    await removeWord(word_id, profile.id);
 
-    
+    const item = getWordByID(word[0].words_to_profile, word_id, profile.id);
+    const index = word[0].words_to_profile.indexOf(item);
+
+    word[0].words_to_profile.splice(index, 1);
 
     display();
 }
 
 async function handleAddWord(word_id) {
-    console.log('updating');
     const dataToUpdate = {
         word_id,
         profile_id: Number(profile.id)
@@ -44,7 +44,6 @@ async function handleAddWord(word_id) {
 
     const data = await saveWord(dataToUpdate);
     data.profiles = { user_id: user.id };
-    console.log(data);
 
     word[0].words_to_profile.push(data);
 
