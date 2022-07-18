682b8b8a957b34eac2a95e806715f15ac4bff956
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-07-17 21:50:16 -0700

    Create profiles on signup

--- services/auth-service.js
+++ services/auth-service.js
@@ -1,11 +1,15 @@
-import { client } from './client.js';
+import { client, checkResponse } from './client.js';
 
 export function getUser() {
     return client.auth.user();
 }
 
 export async function signUp(email, password) {
-    return await client.auth.signUp({ email, password });
+    const response = await client.auth.signUp({ email, password });
+
+    if (response.error) return null;
+    await createProfile(response.user);
+    return response;
 }
 
 export async function signIn(email, password) {
@@ -14,4 +18,17 @@ export async function signIn(email, password) {
 
 export async function signOut() {
     return await client.auth.signOut();
+}
+
+export async function createProfile(user) {
+    const defaultUsername = user.email.match(/.+(?=@)/)[0];
+
+    const response = await client
+        .from('profiles')
+        .insert({
+            user_id: user.id,
+            username: defaultUsername
+        });
+
+    return checkResponse(response);
 }
