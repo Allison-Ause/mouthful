e49ac5eaed90f993f15daf532468b600d0476290
Allison Ause <allison.ause@gmail.com>
2022-07-19 11:58:34 -0700

    styled profiles page for consistency

--- components/ProfileBox.js
+++ components/ProfileBox.js
@@ -3,14 +3,14 @@ export default function createProfileBox(root) {
     const usernameDisplay = root.querySelector('.username');
 
     return ({ profile }) => {
-        usernameDisplay.textContent = profile.username;
+        usernameDisplay.textContent = 'Chef\xa0\xa0' + profile.username + '\'s \xa0Pantry';
     };
 }
 
 function initialize(root) {
     root.innerHTML = '';
 
-    const usernameDisplay = document.createElement('p');
+    const usernameDisplay = document.createElement('h1');
     usernameDisplay.classList.add('username');
     root.append(usernameDisplay);
 }
